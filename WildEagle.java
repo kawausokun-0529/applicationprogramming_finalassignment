d792cc9c4d9ac54b5b68e19bb49bb3795d1e6ab9
kawausokun-0529 <138553784+kawausokun-0529@users.noreply.github.com>
2024-01-28 04:57:13 +0900

    Add files via upload

---
+++ WildEagle.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+
+public class WildEagle extends Piece{
+    public WildEagle(int owner, boolean promoted, int x, int y){
+        super(owner, promoted);
+        this.pieceID = 198;
+        this.position = new Coordinate(x, y);
+    }
+
+    ArrayList<Coordinate> getPossibleMoves(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        
+        switch(owner){
+            case 0:
+            case 1:
+                while(canMoveInDirection(board, xCo-1, yCo)){
+                    if(board.getPiece(xCo-1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo-1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo-1, yCo));
+                    xCo = xCo - 1;
+                }
+                xCo = position.getX();
+                while(canMoveInDirection(board, xCo+1, yCo+1)){
+                    if(board.getPiece(xCo+1, yCo+1) != null){
+                        possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                    xCo = xCo + 1;
+                    yCo = yCo + 1;
+                }
+                xCo = position.getX();
+                yCo = position.getY();
+                while(canMoveInDirection(board, xCo, yCo+1)){
+                    if(board.getPiece(xCo, yCo+1) != null){
+                        possibleMoves.add(new Coordinate(xCo, yCo+1));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo, yCo+1));
+                    yCo = yCo + 1;
+                }
+                yCo = position.getY();
+                while(canMoveInDirection(board, xCo+1, yCo-1)){
+                    if(board.getPiece(xCo+1, yCo-1) != null){
+                        possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                    xCo = xCo + 1;
+                    yCo = yCo - 1;
+                }
+                xCo = position.getX();
+                yCo = position.getY();
+                break;
+        }
+
+        return possibleMoves;
+    }
+
+    public ArrayList<Coordinate> promote(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        
+        switch(owner){
+            case 0:
+            case 1:
+                while(canMoveInDirection(board, xCo-1, yCo)){
+                    if(board.getPiece(xCo-1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo-1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo-1, yCo));
+                    xCo = xCo - 1;
+                }
+                xCo = position.getX();
+                while(canMoveInDirection(board, xCo+1, yCo)){
+                    if(board.getPiece(xCo+1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo+1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo+1, yCo));
+                    xCo = xCo + 1;
+                }
+                xCo = position.getX();
+                if(canMoveInDirection(board, xCo+2, yCo+2)){
+                    possibleMoves.add(new Coordinate(xCo+2, yCo+2));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo+2, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo+2, yCo-2));
+                }
+                if(canMoveInDirection(board, xCo, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-2));
+                }
+                
+                break;
+        }
+
+        return possibleMoves;
+    }
+}
