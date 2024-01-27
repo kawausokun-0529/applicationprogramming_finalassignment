54e0f1d761d93627faf3b911c952908f71305641
kawausokun-0529 <138553784+kawausokun-0529@users.noreply.github.com>
2024-01-28 04:50:49 +0900

    Add files via upload

---
+++ FierceDeer.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+
+public class FierceDeer extends Piece{
+    public FierceDeer(int owner, boolean promoted, int x, int y){
+        super(owner, promoted);
+        this.pieceID = 55;
+        this.position = new Coordinate(x, y);
+    }
+
+    ArrayList<Coordinate> getPossibleMoves(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        
+        switch(owner){
+            case 0:
+            case 1:
+                while(canMoveInDirection(board, xCo-1, yCo)){
+                    if(board.getPiece(xCo-1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo-1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo-1, yCo));
+                    xCo = xCo - 1;
+                }
+                xCo = position.getX();
+                while(canMoveInDirection(board, xCo+1, yCo)){
+                    if(board.getPiece(xCo+1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo+1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo+1, yCo));
+                    xCo = xCo + 1;
+                }
+                xCo = position.getX();
+                if(canMoveInDirection(board, xCo+1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-2));
+                }
+                if(canMoveInDirection(board, xCo, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo, yCo+2)){
+                    possibleMoves.add(new Coordinate(xCo, yCo+2));
+                }
+                break;
+        }
+
+        return possibleMoves;
+    }
+
+    public ArrayList<Coordinate> promote(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        
+        switch(owner){
+            case 0:
+            case 1:
+                while(canMoveInDirection(board, xCo-1, yCo)){
+                    if(board.getPiece(xCo-1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo-1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo-1, yCo));
+                    xCo = xCo - 1;
+                }
+                xCo = position.getX();
+                while(canMoveInDirection(board, xCo+1, yCo)){
+                    if(board.getPiece(xCo+1, yCo) != null){
+                        possibleMoves.add(new Coordinate(xCo+1, yCo));
+                        break;
+                    }
+                    possibleMoves.add(new Coordinate(xCo+1, yCo));
+                    xCo = xCo + 1;
+                }
+                xCo = position.getX();
+                if(canMoveInDirection(board, xCo+2, yCo+2)){
+                    possibleMoves.add(new Coordinate(xCo+2, yCo+2));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo+2, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo+2, yCo-2));
+                }
+                if(canMoveInDirection(board, xCo, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-2));
+                }
+                if(canMoveInDirection(board, xCo, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-2)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-2));
+                }
+                break;
+        }
+
+        return possibleMoves;
+    }
+}
---
+++ FierceTiger.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class FierceTiger extends Piece{
+    public FierceTiger(int owner, boolean promoted, int x, int y){
+        super(owner, promoted);
+        this.pieceID = 59;
+        this.position = new Coordinate(x, y);
+    }
+
+    ArrayList<Coordinate> getPossibleMoves(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        switch (owner){
+            case 0:
+            case 1:
+                if(canMoveInDirection(board, xCo+1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo-1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo-1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo-1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo-1, yCo+1));
+                }
+        }
+
+        return possibleMoves;
+    }
+
+    public ArrayList<Coordinate> promote(Surface board){
+        ArrayList<Coordinate> possibleMoves = new ArrayList<>();
+        int xCo = position.getX();
+        int yCo = position.getY();
+        
+        switch(owner){
+            case 0:
+            case 1:
+                if(canMoveInDirection(board, xCo+1, yCo)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo+1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo+1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo-1, yCo+1)){
+                    possibleMoves.add(new Coordinate(xCo-1, yCo+1));
+                }
+                if(canMoveInDirection(board, xCo-1, yCo-1)){
+                    possibleMoves.add(new Coordinate(xCo-1, yCo-1));
+                }
+                if(canMoveInDirection(board, xCo-1, yCo)){
+                    possibleMoves.add(new Coordinate(xCo-1, yCo));
+                }
+        }
+
+        return possibleMoves;
+    }
+}
